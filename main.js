5f9efc0e4b79e24af8fcc6afd09e72d81d6c9744
Zachary Fong <zacharyfong@Zacharys-MacBook-Pro.local>
2016-08-10 20:57:40 -0700

    created game logic

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("JS file is connected to HTML! Woo!")
+
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log(cardOne === cardTwo);
+console.log(cardOne == cardFour);
