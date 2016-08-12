92effa0399db90ab2aef4f6a013ae9e4e00aa5d6
Zachary Fong <zacharyfong@Zacharys-MacBook-Pro.local>
2016-08-12 03:06:25 -0700

    unit10Assignment

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("JS file is connected to HTML! Woo!")
+
 
 
 var cardOne = "queen";
@@ -6,13 +6,27 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-console.log(cardOne === cardTwo);
-console.log(cardOne == cardFour);
-
-if (cardOne === cardTwo) {
+//console.log(cardOne === cardTwo);
+//console.log(cardOne == cardFour);
+/*if (cardOne === cardTwo) {
 	alert('You found a match!');
 } else if (cardThree === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
+}*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function(){
+
+for (var i=0; i<4; i++){
+	var createDiv = document.createElement('div');
+	createDiv.className = 'card';
+    gameBoard.appendChild(createDiv);
 }
+
+
+
+}
+createBoard();
